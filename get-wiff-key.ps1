79317e6cf94597c8d73c62318960190122b99f75
Steven guimaraes <99700348+ticofookfook@users.noreply.github.com>
2023-11-08 10:15:54 -0300

    Update get-wiff-key.ps1

--- get-wiff-key.ps1
+++ get-wiff-key.ps1
@@ -1,15 +1,19 @@
-$profiles = netsh wlan show profiles | Select-String "Todos os Perfis de Usuários:"
+$profiles = netsh wlan show profiles | ForEach-Object {
+    $splitResult = $_ -split ":", 2
+    if ($splitResult.Count -gt 1) {
+        $splitResult[1].Trim()
+    }
+}
 
 foreach ($profile in $profiles) {
-  $profileName = $profile -replace "Todos os Perfis de Usuários: ", ""
-  $profileInfo = netsh wlan show profile name="$profileName" key=clear
-  $matches = $profileInfo | Select-String -Pattern "Nome SSID\s+:\s+(.+)$|Conteúdo da Chave\s+:\s+(.+)$"
-  if ($matches.Count -eq 2) {
-    $nomeRede = $matches[0].Matches.Groups[1].Value
-    $conteudoChave = $matches[1].Matches.Groups[2].Value
-    Write-Host "Nome da Rede: $nomeRede"
-    Write-Host "Senha: $conteudoChave"
-  } else {
-    Write-Host "Informações não encontradas para o perfil $profileName."
-  }
+    $profileInfo = netsh wlan show profile name="$profile" key=clear
+    $matches = $profileInfo | Select-String -Pattern "Nome SSID\s+:\s+(.+)$|Conteúdo da Chave\s+:\s+(.+)$"
+    if ($matches.Count -eq 2) {
+        $nomeRede = $matches[0].Matches.Groups[1].Value
+        $conteudoChave = $matches[1].Matches.Groups[2].Value
+        Write-Host "Nome da Rede: $nomeRede"
+        Write-Host "Senha: $conteudoChave"
+    } else {
+        Write-Host "Informações não encontradas para o perfil $profile."
+    }
 }
