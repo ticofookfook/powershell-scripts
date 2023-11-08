90aa145edf4298dd0880ec6d7783922b2d716cb2
Steven guimaraes <99700348+ticofookfook@users.noreply.github.com>
2023-11-08 10:55:29 -0300

    Update get-wiff-key.ps1

--- get-wiff-key.ps1
+++ get-wiff-key.ps1
@@ -17,3 +17,9 @@ foreach ($profile in $profiles) {
         Write-Host "Informações não encontradas para o perfil $profile."
     }
 }
+
+
+
+
+####codigo em 1 linha#######
+netsh wlan show profiles | ForEach-Object { $splitResult = ($_ -split ":", 2); if ($splitResult.Count -gt 1) { $profile = $splitResult[1].Trim(); if ($profile) { $matches = (netsh wlan show profile name="$profile" key=clear) | Select-String -Pattern "Nome SSID\s+:\s+(.+)$|Conteúdo da Chave\s+:\s+(.+)$"; if ($matches.Count -eq 2) { $nomeRede, $conteudoChave = $matches[0].Matches.Groups[1].Value, $matches[1].Matches.Groups[2].Value; Write-Host "Nome da Rede: $nomeRede"; Write-Host "Senha: $conteudoChave"; } else { Write-Host "Informações não encontradas para o perfil $profile." } } } }
